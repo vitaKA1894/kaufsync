6da1a60111ede25202ba2f354af145c2ea888093
Vita <vitali.kotrini@gmail.com>
2026-07-15 14:03:44 +0200

    Merge Jules changes

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.2821fj1digo"
+    "revision": "0.41rvtsalkp"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
