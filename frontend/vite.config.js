4647b261e950674233eb20cd0492aa93ae70a210
Vita <vitali.kotrini@gmail.com>
2026-08-06 12:54:15 +0200

    Fix Logo & Header

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,55 +1,38 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8000',
-      '/ws': {
-        target: 'ws://localhost:8000',
-        ws: true
-      }
-    }
-  },
   plugins: [
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
-      injectRegister: 'auto',
-      includeAssets: ['favicon.svg', 'icons.svg', 'pwa-192x192.png', 'pwa-512x512.png'],
-      
-      // NEU: Zwingt Vite, die PWA auch im dev-Modus zu testen!
-      devOptions: {
-        enabled: true
-      },
-      
+      includeAssets: ['favicon.svg', 'pwa-192x192.png', 'pwa-512x512.png'],
       manifest: {
         name: 'KaufSync',
         short_name: 'KaufSync',
-        description: 'Private und hochperformante Einkaufsliste',
-        theme_color: '#2c3e50',
-        background_color: '#ffffff',
+        title: 'KaufSync',
+        theme_color: '#863bff',
+        background_color: '#08060d',
         display: 'standalone',
+        orientation: 'portrait',
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: 'pwa-192x192.png',
             sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any maskable'
+            type: 'image/png'
           },
           {
-            src: '/pwa-512x512.png',
+            src: 'pwa-512x512.png',
             sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
+            type: 'image/png'
           }
         ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-		navigateFallback: '/index.html' // NEU: Leitet alle Offline-Requests auf die App um
       }
     })
-  ]
-})
+  ],
+  server: {
+    host: true,
+    port: 5173
+  }
+});
